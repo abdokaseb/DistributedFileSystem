81dc8396924710b73453341a101c2ce4bb1c5ef9
abdokaseb <abdokaseb@gmail.com>
2019-04-11 06:12:49 +0200

    make the database for look up data

---
+++ MasterTracker/lookUpTableInit.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import sys
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS lookUpData")
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="lookUpData"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS machines (ID Int, isAlive Int, IP VARCHAR(13), primary key(ID))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS files (UserID Int, machID Int, fileName VARCHAR(255))")
+
+# sql = ''
+mainSQL = "INSERT INTO machines (ID, isAlive) VALUES (%s, %s);"
+
+for i in range(1000):
+  # sql += mainSQL.format(i)
+  mycursor.execute(mainSQL,(str(i),'0'))
+  mydb.commit()
+
+
+
